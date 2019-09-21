ea943c3ff4d642b1b55e6bcd85b1fde36f38d386
li huan <li.huan@zhtu.net>
2019-09-21 21:20:09 +0800

    add map

--- src/app/pages/map/map.ts
+++ src/app/pages/map/map.ts
@@ -3,6 +3,9 @@ import { ConferenceData } from '../../providers/conference-data';
 import { Platform } from '@ionic/angular';
 import { environment } from '../../../environments/environment';
 
+const win = window as any;
+const googleModule = win.google;
+
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
@@ -11,16 +14,41 @@ import { environment } from '../../../environments/environment';
 export class MapPage implements AfterViewInit {
   @ViewChild('mapCanvas', { static: true }) mapElement: ElementRef;
 
-  constructor(public confData: ConferenceData, public platform: Platform) {}
+  constructor(public confData: ConferenceData, public platform: Platform) { }
 
   async ngAfterViewInit() {
     const googleMaps = await getGoogleMaps(environment.Map.api_key);
-    this.confData.getMap().subscribe((mapData: any) => {
+    this.confData.getMap().subscribe(async (mapData: any) => {
       const mapEle = this.mapElement.nativeElement;
-
+      // navigator.geolocation.getCurrentPosition(position => {
+      //   let coords = position.coords;
+      //   let latlng = new googleModule.maps.LatLng(coords.latitude, coords.longitude);
+      //   console.log('getlaglng', latlng);
+      // })
+      let loaction, latlng
+      try {
+        console.log('start get position.......');
+        let location = await getPosition();
+        latlng = new googleModule.maps.LatLng(location.lat, location.lng);
+        console.log('get success', location);
+      } catch (err) {
+        latlng = {
+          lat: 39.907333,
+          lng: 116.391083,
+          center: true
+        }
+        console.log('getPosition err',err)
+      }
       const map = new googleMaps.Map(mapEle, {
-        center: mapData.find((d: any) => d.center),
-        zoom: 16
+        // center: mapData.find((d: any) => d.center),
+        center: latlng,
+        zoom: 16,
+        zoomControl: true,
+        mapTypeControl: true,
+        scaleControl: true,
+        streetViewControl: true,
+        rotateControl: true,
+        fullscreenControl: true
       });
 
       mapData.forEach((markerData: any) => {
@@ -46,9 +74,8 @@ export class MapPage implements AfterViewInit {
   }
 }
 
+
 function getGoogleMaps(apiKey: string): Promise<any> {
-  const win = window as any;
-  const googleModule = win.google;
   if (googleModule && googleModule.maps) {
     return Promise.resolve(googleModule.maps);
   }
@@ -71,3 +98,17 @@ function getGoogleMaps(apiKey: string): Promise<any> {
     };
   });
 }
+
+function getPosition(): Promise<any> {
+  return new Promise((resolve, reject) => {
+
+    navigator.geolocation.getCurrentPosition(resp => {
+      resolve({ lng: resp.coords.longitude, lat: resp.coords.latitude });
+    },
+      err => {
+        reject(err);
+      }, {timeout: 5000});
+  });
+
+}
+
